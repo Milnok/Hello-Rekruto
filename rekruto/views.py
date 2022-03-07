3737a2dfeeafaaebbcb5383b6e70f4f869840788
Milnok <vip.egorenko@mail.ru>
2022-03-07 20:55:49 +0700

    exception handle

--- rekruto/views.py
+++ rekruto/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 def home(request):
-    data = {
-        'name': request.GET['name'],
-        'message': request.GET['message'],
-    }
+    try:
+        data = {
+            'name': request.GET['name'],
+            'message': request.GET['message'],
+        }
+    except MultiValueDictKeyError:
+        return render(request, 'rekruto/index.html')
     return render(request, 'rekruto/index.html', data)
